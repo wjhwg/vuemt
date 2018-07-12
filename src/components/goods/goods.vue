ca973a2e3980c579b6879aeb13df683d9492d5b9
hwg <1466619197@qq.com>
2018-07-12 13:15:01 +0800

    goods layout

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,51 @@
 <template>
   <div class="goods">
-    goods
+    <div class="menu-wrapper">
+
+    </div>
+    <div class="foods-wrapper">
+
+    </div>
   </div>
 </template>
 <script>
+const ERR_OK = 0
 export default {
-  name: 'goods'
+  name: 'goods',
+  data () {
+    return {
+      goods: []
+    }
+  },
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  created () {
+    this.$http.get('/api/goods').then((response) => {
+      response = response.body
+      if (response.errno === ERR_OK) {
+        this.goods = response.data
+        console.log(this.goods)
+      }
+    })
+  }
 }
 </script>
-<style scoped>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+  .goods
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background: #f3f5f7
+    .foods-wrapper
+      flex 1
 </style>
